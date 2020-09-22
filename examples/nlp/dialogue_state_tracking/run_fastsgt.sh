c9e07740f9fb6b2c70c8cdea6785a94d6a020798
glhr <grenblot@gmail.com>
2020-09-22 07:10:24 +0200

    added script for running fastsgt

---
+++ examples/nlp/dialogue_state_tracking/run_fastsgt.sh
@@ -0,0 +1,1 @@
+dialogue_state_tracking_sgd.py --data_dir ~/dstc8-schema-guided-dialogue --tracker_model=nemotracker --checkpoint_dir=output/SGD/fastsgt/checkpoints --no_overwrite_schema_emb_files --no_overwrite_dial_files --no_time_to_log_dir --work_dir=output/SGD/fastsgt
