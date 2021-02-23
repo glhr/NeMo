027ccd7dd6a104c5ea46d7bcea7e9c995c3ec2c5
glhr <grenblot@gmail.com>
2021-02-23 13:11:42 +0100

    jetson backup

---
+++ examples/nlp/dialogue_state_tracking/run_fastsgt.sh
@@ -0,0 +1,2 @@
+LD_PRELOAD=/usr/lib/aarch64-linux-gnu/libtcmalloc_minimal.so.4 python dialogue_state_tracking_sgd.py --data_dir ~/dstc8-schema-guided-dialogue --tracker_model=nemotracker --task_name=dstc8_multi_domain --no_overwrite_schema_emb_files --no_overwrite_dial_files --checkpoint_dir /home/xavier/NeMo/examples/nlp/dialogue_state_tracking/output/SGD/2020-09-22_07-16-27/checkpoints --work_dir /home/xavier/NeMo/examples/nlp/dialogue_state_tracking/output/SGD/2020-09-22_07-16-27 --no_time_to_log_dir
+
